04664c1f7f548a5fb225f54ef17effe35365a2b7
leejihoo <ajtwlsdl33@gmail.com>
2025-04-17 13:15:49 +0900

    [disjoint_set] 1717 집합의 표현

---
+++ src/1717.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<string>
+#include <vector>
+
+using namespace std;
+
+int findParent(vector<int> & parent, int target){
+    if(parent[target] == target){
+        return target;
+    }
+
+    return parent[target] = findParent(parent, parent[target]);
+}
+
+void unionParent(vector<int> & parent, int a, int b){
+    int parentA = findParent(parent,a);
+    int parentB = findParent(parent,b);
+
+    if(parentA == parentB){
+        return;
+    }
+
+    if(parentA < parentB){
+        parent[parentB] = parentA;
+    }
+    else{
+        parent[parentA] = parentB;
+    }
+}
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n,m;
+    cin >> n >> m;
+    vector<int> parent;
+    for(int i = 0; i<n+1; i++){
+        parent.push_back(i);
+    }
+
+    for(int i = 0; i<m; i++){
+        int command,a,b;
+        cin >> command >> a >> b;
+        
+        if(command == 0){ // 합집합
+            unionParent(parent, a, b);
+        }
+        else{ // command == 1 부모가 같은지 확인
+            if(findParent(parent,a) == findParent(parent,b)){
+                cout << "yes" << endl;
+            }
+            else{
+                cout << "no" << endl;
+            }
+        }
+    }
+}
